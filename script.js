358f7ea04b02e8e149e179de9e79b75b67c550c8
Chirag Jain <chiragjaindev.com>
2023-02-18 22:38:00 +0530

    added register button

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// document.getElementsByTagName('main')[0].style.height = `${
-//     window.innerHeight - document.getElementsByTagName('nav')[0].clientHeight
-// }`;
+var animateButton = function(e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
