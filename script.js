ecbfd4caa4bc57d9c79345fc3ea81bb652ea4ecb
Chirag Jain <chiragjaindev.com>
2023-02-19 23:40:14 +0530

    added loading animation

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-var animateButton = function (e) {
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
+// var animateButton = function (e) {
+//     e.preventDefault;
+//     //reset animation
+//     e.target.classList.remove('animate');
 
-    e.target.classList.add('animate');
-    setTimeout(function () {
-        e.target.classList.remove('animate');
-    }, 700);
-};
-
-var bubblyButtons = document.getElementsByClassName('bubbly-button');
-
-for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-}
-
-// let scrollTimeout;
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//     var currentScrollPos = window.pageYOffset;
-//     if (prevScrollpos > currentScrollPos) {
-//         document.getElementById('navbar').style.top = '0';
-//     } else {
-//         document.getElementById('navbar').style.top = '-100%';
-//     }
-
-//     // Event buffering here
-//     clearTimeout(scrollTimeout);
-//     scrollTimeout = setTimeout(function () {
-//         prevScrollpos = currentScrollPos;
-//     }, 500); // This delay may need adjustment...
+//     e.target.classList.add('animate');
+//     setTimeout(function () {
+//         e.target.classList.remove('animate');
+//     }, 700);
 // };
+
+// var bubblyButtons = document.getElementsByClassName('bubbly-button');
+
+// for (var i = 0; i < bubblyButtons.length; i++) {
+//     bubblyButtons[i].addEventListener('click', animateButton, false);
+// }
+
