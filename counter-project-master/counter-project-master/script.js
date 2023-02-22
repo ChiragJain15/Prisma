e4b0c1dd787906679ee53e73dd905008e09e9838
Chirag Jain <chiragjaindev.com>
2023-02-22 23:16:20 +0530

    Completed registeration frontend

--- counter-project-master/counter-project-master/script.js
+++ counter-project-master/counter-project-master/script.js
@@ -1,19 +1,3 @@
-const hamburger = document.getElementById('hamburger');
-const nav = document.getElementsByClassName('mob-nav')[0];
-const empty = document.getElementsByClassName('hidden')[0];
-
-hamburger.addEventListener('click', () => {
-    if (nav.classList.contains('show')) {
-        nav.classList.remove('show');
-        setTimeout(() => {
-            empty.classList.remove('show2');
-        }, 3000);
-    } else {
-        empty.classList.add('show2');
-        nav.classList.add('show');
-    }
-});
-
 const days = document.getElementById('days');
 const minutes = document.getElementById('minutes');
 const hours = document.getElementById('hours');
